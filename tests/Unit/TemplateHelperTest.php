fcf8be4291dc458af826be518d7db8081652b7bd
Md. Razu Haolader <razu.cse129@gmail.com>
2023-04-11 12:24:56 +0600

    Update test case

--- tests/Unit/TemplateHelperTest.php
+++ tests/Unit/TemplateHelperTest.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types=1);
+declare( strict_types=1 );
 
 namespace MRH\AuthorBox\Tests\Unit;
 
@@ -8,29 +8,25 @@ use MRH\AuthorBox\Helpers\Template as TemplateHelper;
 use RuntimeException;
 use Yoast\PHPUnitPolyfills\TestCases\TestCase;
 
-class TemplateHelperTest extends TestCase
-{
+class TemplateHelperTest extends TestCase {
 
     private static $template_helper;
 
-    public static function set_up_before_class(): void
-    {
+    public static function set_up_before_class(): void {
         static::$template_helper = new TemplateHelper();
     }
 
-    public function test_should_render_template_when_valid_path_given()
-    {
+    public function test_should_render_template_when_valid_path_given() {
         $this->expectNotToPerformAssertions();
         ob_start();
         $file_path = MRHAB_INCLUDES . '/Frontend/views/enquiry-form.php';
-        static::$template_helper->render($file_path, ['posts' => []]);
+        static::$template_helper->render( $file_path, ['posts' => []] );
         ob_get_clean();
     }
 
-    public function test_should_throw_error_when_invalid_path_given()
-    {
-        $this->expectException(RuntimeException::class);
+    public function test_should_throw_error_when_invalid_path_given() {
+        $this->expectException( RuntimeException::class );
         $file_path = MRHAB_INCLUDES . '/Frontend/templates/test.php';
-        static::$template_helper->render($file_path);
+        static::$template_helper->render( $file_path );
     }
 }
